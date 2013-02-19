fda36b71f2464943fa4bbee13fd03a9b7e89bb62
Chris Wyatt <clwyatt@vt.edu>
2013-02-19 16:36:25 -0500

    Add temporal filter to resting state data
    Added a simple frequency domain rect filter used in conventional
    resting state analysis. It is performed after the nuisance regression.

--- src/regress.py
+++ src/regress.py
@@ -19,12 +19,28 @@ if __name__ == "__main__":
     X = np.hstack((X, wm_sig.reshape(wm_sig.shape[0], -1)))
     X = np.hstack((X, csf_sig.reshape(csf_sig.shape[0], -1)))
 
+    # perform regression on X, result is the residual
     Y = data[global_mask].T
     B = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(Y)
     R = Y - X.dot(B)
 
+
+    # filter the residual using rectangular bandpass 0.009 to 0.08 Hz
+    hdr = inimg.get_header()
+    TR = hdr["pixdim"][4]
+    freq = np.fft.fftfreq(R.shape[0], d = TR).reshape(R.shape[0], -1)
+    fft = np.fft.fft(R, axis = 0)
+
+    filtcoef = np.logical_or(np.logical_and(np.greater(freq, 0.009),
+                                            np.less(freq, 0.08)),
+                             np.logical_and(np.greater(freq, -0.08),
+                                            np.less(freq, -0.009))).astype(np.float64)
+    filtmat = np.dot(filtcoef, np.ones([1, R.shape[1]]))
+    filtered = np.real(np.fft.ifft(np.multiply(fft, filtmat), axis = 0))
+
+    # place back into image
     data[:] = 0
-    data[global_mask] = R.T
+    data[global_mask] = filtered.T
 
     outimg = nb.Nifti1Image(data, header=inimg.get_header(), affine=inimg.get_affine())
     outimg.to_filename('raw_roi_timecorr_mcf_res.nii.gz')
